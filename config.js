cafb3034cfbce3540cc65cba2897a913829ae19c
lequochuy21 <106541407+lequochuy21@users.noreply.github.com>
2022-05-30 21:33:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,4 +17,4 @@ const CONFIG = {
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+          
